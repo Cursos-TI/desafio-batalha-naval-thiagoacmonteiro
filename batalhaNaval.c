de2623e1d678f115982817feebb92da4d1d05b4c
Thiago Monteiro <87547650+thiagoacmonteiro@users.noreply.github.com>
2025-06-25 03:12:46 +0000

    :sparkles:feat: Desafio Completo (Nível Novato)

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,138 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+
+
+void popula_tabuleiro(int tabuleiro[10][10]) {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      tabuleiro[i][j] = 0;
+    }
+  }
+}
+
+void verifica_limites_do_tabuleiro(int posicao_inicial_navio_1, int posicao_inicial_navio_2) {
+  if (posicao_inicial_navio_1 + 3 > 10) {
+    printf("A posição do navio 1 estrapola os limites do tabuleiro");
+    return 0;
+  }
+
+  if (posicao_inicial_navio_2 + 3 > 10) {
+    printf("A posição do navio 2 estrapola os limites do tabuleiro");
+    return 0;
+  }
+}
+
+void posiciona_navio(
+  int num_navio,
+  char colunas[10],
+  int tabuleiro[10][10],
+  int navio_1[3],
+  int navio_2[3],
+  int indice_linha_navio_1,
+  int posicao_inicial_navio_1,
+  int indice_coluna_navio_2,
+  int posicao_inicial_navio_2
+) {
+  switch (num_navio) {
+    case 1:
+      // Iterando sobre as linhas do tabuleiro
+      for (int i = 0; i < 10; i++) {
+        // Verifica se a linha atual é a linha do navio 1
+        if (i == indice_linha_navio_1) {
+          // Iterando sobre as colunas do tabuleiro
+          for (int j = 0; j < 10; j++) {
+            // Verifica se a coluna atual está dentro do intervalo do navio 1
+            if (j >= posicao_inicial_navio_1 && j < posicao_inicial_navio_1 + 3) {
+              // Verifica se já existe um navio na posição atual
+              if (tabuleiro[i][j] != 0) {
+                printf("A posição dos navios gerou conflito na seguinte posição:\n");
+                printf("Coluna: %c\n", colunas[j]);
+                printf("Linha: %d\n", i + 1);
+                return 0;
+              }
+              
+              // Atribui o valor do navio 1 à posição atual do tabuleiro
+              tabuleiro[i][j] = navio_1[j - posicao_inicial_navio_1];
+            }               
+          }
+        }
+      }
+      break;
+    case 2:
+      // Iterando sobre as linhas do tabuleiro
+      for (int i = 0; i < 10; i++) {
+        // Iterando sobre as colunas do tabuleiro
+        for (int j = 0; j < 10; j++) {
+          // Verifica se a coluna atual é a coluna do navio 2
+          // e se a linha atual está dentro do intervalo do navio 2
+          if (j == indice_coluna_navio_2 && i >= posicao_inicial_navio_2 && i < posicao_inicial_navio_2 + 3) {   
+            // Verifica se já existe um navio na posição atual             
+            if (tabuleiro[i][j] != 0) {
+              printf("A posição dos navios gerou conflito na seguinte posição:\n");
+              printf("Coluna: %c\n", colunas[j]);
+              printf("Linha: %d\n", i + 1);
+              return 0;
+            }
+
+            // Atribui o valor do navio 2 à posição atual do tabuleiro
+            tabuleiro[i][j] = navio_2[i - posicao_inicial_navio_2];
+          }
+        }
+      }
+      break;
+  }  
+}
+
+void imprime_tabuleiro(int tabuleiro[10][10]) {
+  printf("   A B C D E F G H I J\n");
+  for (int i = 0; i < 10; i++) {
+    printf(i < 9 ? " %d " : "%d ", i + 1);
+    for (int j = 0; j < 10; j++) {
+      printf("%d ", tabuleiro[i][j]);
+    }
+    printf("\n");
+  }
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+  char colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+  int tabuleiro[10][10];
+  int navio_1[3] = {3, 3, 3};
+  int navio_2[3] = {3, 3, 3};
+  int indice_linha_navio_1 = 3;
+  int indice_coluna_navio_2 = 3;
+  int posicao_inicial_navio_1 = 3;
+  int posicao_inicial_navio_2 = 7;
+  
+  popula_tabuleiro(tabuleiro);
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+  verifica_limites_do_tabuleiro(posicao_inicial_navio_1, posicao_inicial_navio_2);
+  
+  posiciona_navio(
+    1,
+    colunas,
+    tabuleiro,
+    navio_1,
+    navio_2,
+    indice_linha_navio_1,
+    posicao_inicial_navio_1,
+    indice_coluna_navio_2,
+    posicao_inicial_navio_2
+  );
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+  posiciona_navio(
+    2,
+    colunas,
+    tabuleiro,
+    navio_1,
+    navio_2,
+    indice_linha_navio_1,
+    posicao_inicial_navio_1,
+    indice_coluna_navio_2,
+    posicao_inicial_navio_2
+  );
+  
+  imprime_tabuleiro(tabuleiro);  
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
-    return 0;
+  return 0;
 }
